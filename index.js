10118d273717ccc0d0df79789b6a388b21540ddf
wanmuhafidzfaldi <wanmuhafidzfaldi@gnail.com>
2020-01-26 14:13:46 +0700

    update port

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const AppServer = require('./src/route/server');
 const appServer = new AppServer();
-const port = 3000;
-
+const port = 8080;
 appServer.server.listen(port, () => {
     const ctx = 'app-listen';
     console.log(ctx, `${appServer.server.name} started, listening at ${appServer.server.url}`, 'initate application');
